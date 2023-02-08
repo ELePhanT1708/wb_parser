224ed208b9ec525a2fa7fc69bda98af66278048b
ELePhanT1708 <elephant1708@gmail.com>
2023-02-08 13:36:36 +0300

    Added json2table transform

---
+++ selenuim_approach/json2table.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from typing import List
+
+import pandas as pd
+
+
+def read_column_names():
+    excel = pd.read_excel('rawExportExcel (16).xlsx')
+    column_names = []
+    for row in excel:
+        column_names.append(row)
+    return column_names
+
+
+def read_json_info():
+    json = pd.read_json('rostkniga_test.json', encoding='windows-1251')
+    list_of_df = []
+    for book in json:
+        add_row_about_book2df(json[book], list_of_df)
+    res_df = pd.concat(list_of_df)
+    res_df.to_excel('output.xlsx')
+    print(res_df)
+
+
+def create_table():
+    column_names = read_column_names()
+    df = pd.DataFrame(columns=column_names)
+    return df
+
+
+def add_row_about_book2df(json_data: dict, list_of_df: List[pd.DataFrame]):
+    column_names = read_column_names()
+    df = pd.DataFrame(columns=column_names)
+
+    img_urls = ', '.join(json_data['pictures'])
+    for column in column_names:
+        if column == 'Медиафайлы':
+            df.loc[0, column] = img_urls
+        elif column == 'Номер карточки':
+            df.loc[0, column] = 0
+        elif column == 'Предмет':
+            df.loc[0, column] = 'Учебники'
+        else:
+            df.loc[0, column] = json_data['Charasteristics'].get(column)
+    print(df)
+    list_of_df.append(df)
+
+
+if __name__ == '__main__':
+    read_json_info()
+    # create_table()
--- selenuim_approach/wb_parser_selenuim.py
+++ selenuim_approach/wb_parser_selenuim.py
@@ -1,3 +1,4 @@
+import json
 from typing import List
 
 from selenium import webdriver
@@ -19,73 +20,124 @@ class Watcher:
         self.service = Service('"C:\\Users\\Рамиль\\PycharmProjects\\wb_parser\\selenuim_approach\\chromedriver.exe"')
         # for ChromeDriver version 79.0.3945.16 or over
         self.options.add_argument("--disable-blink-features=AutomationControlled")
+        self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        self.options.add_experimental_option('useAutomationExtension', False)
         # self.options.add_argument("--headless")
         # self.options.add_argument("--window-size=%s" % WINDOW_SIZE)
         # user-agent
         self.options.add_argument(
-            "user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
+            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/109.0.0.0 Safari/537.36")
         self.options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(
             service=self.service,
             options=self.options
         )
+        self.driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            'source': '''
+                delete window.cdc_adoQpoasnfa76pfcZLmcfl_Array;
+                delete window.cdc_adoQpoasnfa76pfcZLmcfl_Promise;
+                delete window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol;
+          '''
+        })
 
     def collect_single_book_page_urls_from_brand_page(self, url_for_all_books_of_one_brand: str) -> List[str]:
-        self.driver.get(url=url_for_all_books_of_one_brand)
         list_of_urls = []
-        books_on_page = self.driver.find_elements(By.CSS_SELECTOR, 'img.j-thumbnail.thumbnail')
-        print(books_on_page)
-        for book in books_on_page:
-            url = book.get_attribute('href')
-            print(url)
+        SCROLL_PAUSE_TIME = 0.1
+        for i in range(1, 2):
+            # количество страниц товаров у бренда нужно глянуть самостоятельно
+            # либо разделить количество товаров на сто и прибавить один к целой части
 
-    def parse_book_page(self, pause: int = 0):
+            self.driver.get(url=url_for_all_books_of_one_brand + f'?&page={i}&all=')
+
+            time.sleep(2)
+
+
+            # Get scroll height
+            last_height = self.driver.execute_script("return document.body.scrollHeight")
+            last_height = 200
+            next_height = 400
+
+            for i in range(60):
+                # Scroll down to bottom
+
+                self.driver.execute_script(f"window.scrollTo({last_height}, {next_height});")
+                last_height = next_height
+                next_height += 200
+                # Wait to load page
+                time.sleep(SCROLL_PAUSE_TIME)
+
+                # # Calculate new scroll height and compare with last scroll height
+                # new_height = self.driver.execute_script("return document.body.scrollHeight")
+                # if new_height == last_height:
+                #     break
+                # last_height = new_height
+            # books_on_page = self.driver.find_elements(By.CSS_SELECTOR, '.product-card')
+            # books_on_page = self.driver.find_elements(By.CLASS_NAME, 'product-card__wrapper')
+            # books_on_page = self.driver.find_elements(by=B)
+            books_on_page = self.driver.find_elements(By.XPATH, "//div/a")
+            # books_on_page = self.driver.find_elements(By.PARTIAL_LINK_TEXT, "https://www.wildberries.ru/catalog/")
+            length = len(books_on_page)
+            for book in books_on_page:
+                url = book.get_attribute('href')
+                if 'catalog' in url:
+                    list_of_urls.append(url)
+
+        print(list_of_urls)
+        print(len(list_of_urls))
+        return list_of_urls
+
+    def parse_book_page(self, book_url: str = 'srs', pause: int = 0):
         # self.driver.get("https://www.wildberries.ru/catalog/115085312/detail.aspx")
-        self.driver.get("https://www.wildberries.ru/catalog/102479872/detail.aspx")
-        time.sleep(pause)
+        self.driver.get(book_url)
+        img_url_list = []
+        characterisitcs = {}
+        time.sleep(0.5)
         buttons_to_open = self.driver.find_elements(By.CSS_SELECTOR, ".collapsable .collapsible__toggle")
         # click on all "Развернуть описание и развернуть характеристики"
         for button in buttons_to_open:
             button.click()
         characteristics = self.driver.find_elements(By.CSS_SELECTOR, ".product-page .product-params__cell-decor span")
         values = self.driver.find_elements(By.CSS_SELECTOR, ".product-page .product-params__table td")
-        for charast, value in zip(characteristics[5:], values[5:]):
-            print(charast.text, ' - ', value.text)
+        price = self.driver.find_elements(By.CSS_SELECTOR, ".product-page .price-block__final-price")[0]
+        description = self.driver.find_elements(By.CSS_SELECTOR, ".collapsable .collapsable__content")[1]
 
+        for charast, value in zip(characteristics[5:], values[5:]):
+            characterisitcs[f'{charast.text}'] = value.text
+            print(charast.text, ' - ', value.text)
+        characterisitcs['Цена'] = int(price.text.split()[0])
+        characterisitcs['Описание'] = description.text
         images = self.driver.find_elements(By.CSS_SELECTOR, "div.slide__content.img-plug.j-wba-card-item")
         for img in images:
             url = img.find_element(By.TAG_NAME, 'img').get_attribute('src')
-            print(url)
+            img_url_list.append(url)
+        print(img_url_list)
+        print(characterisitcs)
+        return {
+            'pictures': img_url_list,
+            'Charasteristics': characterisitcs
+        }
 
 
-# class Client:
-#     def __init__(self):
-#         self.session = requests.Session()
-#         self.session.headers = {
-#             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-#                           'Chrome/109.0.0.0 Safari/537.36',
-#         }
-#
-#     def load_page(self) -> str:
-#         url = 'https://www.wildberries.ru/catalog/knigi/uchebnaya-literatura#c115085312'
-#         res = self.session.get(url=url)
-#         res.raise_for_status()
-#         return res.text
-#
-#     def parse_page(self, text: str):
-#         soup = bs4.BeautifulSoup(text, 'lxml')
-#         container = soup.select('div')
-#         for block in container:
-#             self.parse_block(block)
-#
-#     def parse_block(self, block):
-#         logger.info(block)
-#         logger.info('='* 100)
-#
-#     def run(self):
-#         text = self.load_page()
-#         self.parse_page(text)
 
 if __name__ == '__main__':
     watcher = Watcher()
-    watcher.collect_single_book_page_urls_from_brand_page('https://www.wildberries.ru/brands/rostkniga/all')
+    brand_url = 'https://www.wildberries.ru/brands/rostkniga/all'
+    list_of_urls = watcher.collect_single_book_page_urls_from_brand_page(brand_url)
+    # with open("rostkniga_all.txt", "w", encoding='windows-1251') as outfile:
+    #     for url in list_of_urls:
+    #         outfile.write(url + '\n')
+    # with open("rostkniga_test.txt", "w", encoding='windows-1251') as outfile:
+    #     for url in list_of_urls:
+    #         outfile.write(url + '\n')
+    json_info = {}
+    for book in list_of_urls:
+        res = watcher.parse_book_page(book)
+        json_info[f'{book}'] = res
+    # Serializing json
+    json_object = json.dumps(json_info, indent=4, ensure_ascii=False)
+    #
+    # Writing to sample.json
+    with open("rostkniga_test.json", "w", encoding='windows-1251') as outfile:
+        outfile.write(json_object)
+
